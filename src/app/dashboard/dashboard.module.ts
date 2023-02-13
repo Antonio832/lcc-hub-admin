9d64dd98f3cf431b86d9a6344e2cc2b092bfd52b
Antonio832 <antoniomedina832@outlook.com>
2023-02-12 18:29:43 -0700

    admin create user dialog created

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -35,10 +35,11 @@ import { AggArticuloDialogComponent } from './dialogs/agg-articulo-dialog.compon
 import { StorageModule } from '@angular/fire/storage';
 import { EditaArtDialogComponent } from './dialogs/edita-art-dialog.component';
 import { ConfirmActionDialogComponent } from './dialogs/confirm-action-dialog.component';
+import { CreateUserDialogComponent } from './dialogs/create-user-dialog.component';
 
 
 @NgModule({
-  declarations: [DashboardComponent, NavComponent, AggAlumnosComponent, PreviewCsvComponent, AggMateriaComponent, AggMapaComponent, AnunciosComponent, ArticulosComponent, VideosComponent, GaleriaComponent, AdminComponent, BuscaAlumnoDialogComponent, BuscaMateriaDialogComponent, EditaPropiedadDialogComponent, AggArticuloDialogComponent, EditaArtDialogComponent, ConfirmActionDialogComponent],
+  declarations: [DashboardComponent, NavComponent, AggAlumnosComponent, PreviewCsvComponent, AggMateriaComponent, AggMapaComponent, AnunciosComponent, ArticulosComponent, VideosComponent, GaleriaComponent, AdminComponent, BuscaAlumnoDialogComponent, BuscaMateriaDialogComponent, EditaPropiedadDialogComponent, AggArticuloDialogComponent, EditaArtDialogComponent, ConfirmActionDialogComponent, CreateUserDialogComponent],
   imports: [
     CommonModule,
     MatButtonModule,
---
+++ src/app/dashboard/dialogs/create-user-dialog.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-user-dialog',
+  template: `
+    <p>
+      create-user-dialog works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class CreateUserDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
